5dd1b57ebe1569ec22f33fa4f22784946064ee16
Hendrik Lösch <loeschhendrik@googlemail.com>
2022-12-11 13:29:42 +0000

    added first service

--- hendrik-loesch-middleware/Services/PublicationTypeService.cs
+++ hendrik-loesch-middleware/Services/PublicationTypeService.cs
@@ -1,8 +1,16 @@
+using Grpc.Core;
 using hendrik_loesch_api;
 
 namespace hendrik_loesch_middleware;
 
 public class TypeService : PublicationTypeService.PublicationTypeServiceBase
 {
+    public override Task<PublicationTypeCollection> GetAll(PublicationTypesRequest request, ServerCallContext context)
+    {
+        var collection = new PublicationTypeCollection();
+        collection.Types_.Add(new PublicationType(){ Name = "Meiner"});
+        collection.Types_.Add(new PublicationType(){ Name = "Noch einer"});
 
+        return base.GetAll(request, context);
+    }
 }
