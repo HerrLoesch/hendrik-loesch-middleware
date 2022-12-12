015f12882a79836717fb6372fa594f2599d630d7
Hendrik Lösch <loeschhendrik@googlemail.com>
2022-12-12 19:10:24 +0000

    Added logger to TypeService

--- hendrik-loesch-middleware/Services/PublicationTypeService.cs
+++ hendrik-loesch-middleware/Services/PublicationTypeService.cs
@@ -5,8 +5,17 @@ namespace hendrik_loesch_middleware;
 
 public class TypeService : PublicationTypeService.PublicationTypeServiceBase
 {
+    private readonly ILogger<TypeService> logger;
+
+    public TypeService(ILogger<TypeService> logger)
+    {
+        this.logger = logger;
+    }
+
     public override Task<PublicationTypeCollection> GetAll(PublicationTypesRequest request, ServerCallContext context)
     {
+        this.logger.LogInformation($"Publication types are requested: {request.ToString()}");
+        
         var collection = new PublicationTypeCollection();
         collection.Types_.Add(new PublicationType(){ Name = "Meiner"});
         collection.Types_.Add(new PublicationType(){ Name = "Noch einer"});
