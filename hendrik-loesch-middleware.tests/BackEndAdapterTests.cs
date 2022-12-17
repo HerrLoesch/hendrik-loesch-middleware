afd732c923db69b433e6e2965e41296c4e8ad40b
Hendrik Lösch <loeschhendrik@googlemail.com>
2022-12-17 14:35:34 +0000

    Added Unit Tests and solution

---
+++ hendrik-loesch-middleware.tests/BackEndAdapterTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace hendrik_loesch_middleware.tests;
+
+public class BackEndAdapterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
